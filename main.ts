9bc8d30fe06f7ddfb5cf1fe76d289473e8d403e9
Koichi <koichii@live.jp>
2019-03-08 05:14:27 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,25 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 icon="\uf1ec" color=#00bbd7 block="そろばん"
+namespace soroban {
+    //% blockId=show_number block="Show a number %num"
+    export function showNumber(num: number = 0): void {
+        let n = num
+        if (n < 0) {
+            led.plot(0, 2)
+            n *= -1
+        }
+        for (let x = 4; x > 0; x--) {
+            let m = n % 10
+            if (m >= 5) {
+                led.plot(x, 0)
+                m -= 5
+            } else {
+                led.unplot(x, 0)
+            }
+            for (let y = 1; y < 5; y++) {
+                (m > 0) ? led.plot(x, y) : led.unplot(x, y)
+                m--
+            }
+            n = Math.floor(n / 10)
+        }
+    }
+}
