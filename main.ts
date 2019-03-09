e149f489875d8ebe06919cca9411e4794d9308c5
yoshidayjp <47466864+yoshidayjp@users.noreply.github.com>
2019-03-09 11:49:16 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=70 icon="\uf1ec" color=#00bbd7 block="そろばん"
+//% weight=70 icon="\uf1ec" color=#00bbd7 block="そろばん2"
 namespace soroban {
     //% blockId=show_number block="かずを ひょうじ %num"
     export function showNumber(num: number = 0): void {
